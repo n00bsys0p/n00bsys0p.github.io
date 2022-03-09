7a0a139099d1489e08d36df5569ea985eb5f8f0f
Alex Shepherd <dev@alexshepherd.me>
2022-03-09 20:51:03 +0000

    Updates 20220309T205103

---
+++ _nuxt/static/1646859052/posts/bright-and-white-responsive-d7-theme/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646859052",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fbright-and-white-responsive-d7-theme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b94614cc"}}}}(null));
---
+++ _nuxt/static/1646859052/posts/mount-partitions-virtualbox-vdi-linux/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646859052",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fmount-partitions-virtualbox-vdi-linux",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b94614cc"}}}}(null));
