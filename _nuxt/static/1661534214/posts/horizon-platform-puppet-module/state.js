c24c5547507b93f4062b086da69f0cd467a2b841
Alex Shepherd <dev@alexshepherd.me>
2022-08-26 17:17:06 +0000

    Updates 20220826T171706

---
+++ _nuxt/static/1661534214/posts/horizon-platform-puppet-module/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661534214",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fhorizon-platform-puppet-module",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8abe0932"}}}}(null));
